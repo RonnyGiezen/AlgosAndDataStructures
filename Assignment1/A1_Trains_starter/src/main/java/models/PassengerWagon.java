4456cde266e139f8fbc56b63cd96c0c81517ff8c
ronny hva <ronny.giezen@hva.nl>
2021-03-03 21:40:42 +0100

    set fresh assignment one and implemented some basics to get classes to work

---
+++ Assignment1/A1_Trains_starter/src/main/java/models/PassengerWagon.java
@@ -0,0 +1,17 @@
+package models;
+// TODO
+public class PassengerWagon extends Wagon {
+    private int numberOfSeats;
+
+    public PassengerWagon(int wagonId, int numberOfSeats) {
+        super(wagonId);
+        this.numberOfSeats = numberOfSeats;
+        // TODO
+    }
+
+    public int getNumberOfSeats() {
+        // TODO
+
+        return 0;
+    }
+}
